7ab6e7422f7286eac7e818b125f40e07d36efaef
davodhiambo <jesseomolo@yahoo.com>
2025-05-14 10:59:49 +0300

    feat: Add custom admin notes dashboard widget
    - Register new dashboard widget for admin notes
    - Create widget with unique slug 'custom_admin_notes_widget'
    - Set up display callback function
    - Hook into wp_dashboard_setup to initialize widget

--- wp-content/plugins/custom-admin-notes/custom-admin-notes.php
+++ wp-content/plugins/custom-admin-notes/custom-admin-notes.php
@@ -6,3 +6,12 @@
  * Author: David Jesse Odhiambo
  * License: GPL2
  */
+
+ function can_add_dashboard_widget() {
+    wp_add_dashboard_widget(
+        'custom_admin_notes_widget',          // Widget slug.
+        'Custom Admin Notes',                 // Title.
+        'can_display_dashboard_widget'        // Display function.
+    );
+}
+add_action('wp_dashboard_setup', 'can_add_dashboard_widget');
