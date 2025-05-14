b97ee0d1639196f7117f7f8afc3bd7667d3a56c2
davodhiambo <jesseomolo@yahoo.com>
2025-05-14 10:01:30 +0300

    headers: update cutsom-admin-notes plugin header.

---
+++ wp-content/plugins/custom-admin-notes/custom-admin-notes.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Custom Admin Notes
+ * Description: Adds a custom dashboard widget for admin-only notes.
+ * Version: 1.0
+ * Author: David Jesse Odhiambo
+ * License: GPL2
+ */
